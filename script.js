eea7522256e3542e02bbc72cd50b8455293312fc
samsterpiece <samantha.badeau@gmail.com>
2022-02-03 15:27:04 -0500

    added styles

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ btnHold.addEventListener('click', function () {
           "Total Score: " +  scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
 
             winMessage.classList.remove('hidden');
 

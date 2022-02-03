3fda9b067a0968f2e13b1caeed3471f8a39d3e89
samsterpiece <samantha.badeau@gmail.com>
2022-02-03 17:46:28 -0500

    fixed winner message

--- script.js
+++ script.js
@@ -166,10 +166,8 @@ btnHold.addEventListener('click', function () {
           "Total Score: " +  scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 100) {
-
+        if (scores[activePlayer] >= 10) {
             winMessage.classList.remove('hidden');
-
             // Finish the game
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');

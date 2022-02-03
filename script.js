f6ca13151704cec87fea683e15dc95b01c754aa3
samsterpiece <samantha.badeau@gmail.com>
2022-02-03 18:23:19 -0500

    fixed style

--- script.js
+++ script.js
@@ -167,6 +167,7 @@ btnHold.addEventListener('click', function () {
 
         // 2. Check if player's score is >= 100
         if (scores[activePlayer] >= 100) {
+            //Display winner message
             winMessage.classList.remove('hidden');
             // Finish the game
             playing = false;
